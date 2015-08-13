abbe2497b75ca0caf4b64e11d5a07ae1ee801a20
Brandon Gonzalez <brandon@iron.io>
2015-08-13 10:18:20 -0700

    replaces worker and api with old iron_go, ports gerred's httpClient stuff over
    Adds back in NewGoCodePackage
    
    Use the original iron_go3 api, modifies worker to be compatible

--- worker/worker.go
+++ worker/worker.go
@@ -29,7 +29,7 @@ func sleepBetweenRetries(previousDuration time.Duration) time.Duration {
 	if previousDuration >= 60*time.Second {
 		return previousDuration
 	}
-	return previousDuration * previousDuration
+	return previousDuration + previousDuration
 }
 
 var GoCodeRunner = []byte(`#!/bin/sh
@@ -45,6 +45,67 @@ cd "$(root "$@")"
 chmod +x worker
 ./worker "$@"
 `)
+
+// WaitForTask returns a channel that will receive the completed task and is closed afterwards.
+// If an error occured during the wait, the channel will be closed.
+func (w *Worker) WaitForTask(taskId string) chan TaskInfo {
+	out := make(chan TaskInfo)
+	go func() {
+		defer close(out)
+		retryDelay := 100 * time.Millisecond
+
+		for {
+			info, err := w.TaskInfo(taskId)
+			if err != nil {
+				return
+			}
+
+			if info.Status == "queued" || info.Status == "running" {
+				time.Sleep(retryDelay)
+				retryDelay = sleepBetweenRetries(retryDelay)
+			} else {
+				out <- info
+				return
+			}
+		}
+	}()
+
+	return out
+}
+
+func (w *Worker) WaitForTaskLog(taskId string) chan []byte {
+	out := make(chan []byte)
+
+	go func() {
+		defer close(out)
+		retryDelay := 100 * time.Millisecond
+
+		for {
+			log, err := w.TaskLog(taskId)
+			if err != nil {
+				e, ok := err.(api.HTTPResponseError)
+				if ok && e.StatusCode() == 404 {
+					time.Sleep(retryDelay)
+					retryDelay = sleepBetweenRetries(retryDelay)
+					continue
+				}
+				return
+			}
+			out <- log
+			return
+		}
+	}()
+	return out
+}
+
+func clamp(value, min, max int) int {
+	if value < min {
+		return min
+	} else if value > max {
+		return max
+	}
+	return value
+}
 
 // NewGoCodePackage creates an IronWorker code package from a Go package.
 // The codeName is the name the code package will have have once it is uploaded.
@@ -101,65 +162,4 @@ func NewGoCodePackage(codeName string, packageArgs ...string) (code Code, err er
 	}
 
 	return
-}
-
-// WaitForTask returns a channel that will receive the completed task and is closed afterwards.
-// If an error occured during the wait, the channel will be closed.
-func (w *Worker) WaitForTask(taskId string) chan TaskInfo {
-	out := make(chan TaskInfo)
-	go func() {
-		defer close(out)
-		retryDelay := 100 * time.Millisecond
-
-		for {
-			info, err := w.TaskInfo(taskId)
-			if err != nil {
-				return
-			}
-
-			if info.Status == "queued" || info.Status == "running" {
-				retryDelay = sleepBetweenRetries(retryDelay)
-				time.Sleep(retryDelay)
-			} else {
-				out <- info
-				return
-			}
-		}
-	}()
-
-	return out
-}
-
-func (w *Worker) WaitForTaskLog(taskId string) chan []byte {
-	out := make(chan []byte)
-
-	go func() {
-		defer close(out)
-		retryDelay := 100 * time.Millisecond
-
-		for {
-			log, err := w.TaskLog(taskId)
-			if err != nil {
-				e, ok := err.(api.HTTPResponseError)
-				if ok && e.StatusCode() == 404 {
-					retryDelay = sleepBetweenRetries(retryDelay)
-					time.Sleep(retryDelay)
-					continue
-				}
-				return
-			}
-			out <- log
-			return
-		}
-	}()
-	return out
-}
-
-func clamp(value, min, max int) int {
-	if value < min {
-		return min
-	} else if value > max {
-		return max
-	}
-	return value
 }
